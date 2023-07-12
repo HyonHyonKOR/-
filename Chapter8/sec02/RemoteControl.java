42af403d4aef1e4534a2a2edd71f23cd751c86d2
HyonHyonKOR <134394081+HyonHyonKOR@users.noreply.github.com>
2023-07-12 22:18:05 +0900

    Add files via upload

---
+++ Chapter8/sec02/RemoteControl.java
@@ -0,0 +1,11 @@
+package ch08.sec02;
+
+public interface RemoteControl {
+	//public 추상 메소드 선언
+	public void turnOn();
+	
+	//RemoteControl rc = new Television();
+    //RemoteControl rc2 = new CoinChangeMachine();  implementをしないクラスのオブジェクトを代入できない。
+
+
+}
---
+++ Chapter8/sec03/RemoteControlExample.java
@@ -0,0 +1,9 @@
+package ch08.sec03;
+
+public class RemoteControlExample implements RemoteControl{
+	public static void main(String[] args) {
+		System.out.println(MAX_VALUE);  //10
+		System.out.println(MIN_VALUE);  //0
+		
+	}
+}
