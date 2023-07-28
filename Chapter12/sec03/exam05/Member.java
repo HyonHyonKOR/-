7becac00a079c09fd949d340ed352b661ef411df
HyonHyon <khm3626@gmail.com>
2023-07-28 23:25:48 +0900

    [20230728] Upload DTO, Lombok

---
+++ Chapter12/sec03/exam05/Member.java
@@ -0,0 +1,17 @@
+package ch12.sec03.exam05;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+
+public class Member {
+	private String id;
+	private String name;
+	private int age;
+	
+
+}
